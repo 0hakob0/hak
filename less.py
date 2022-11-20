f0999bb6c7d200935be06e20bd9c5104bff81aaa
0hakob0 <0hakob0@gmail.com>
2022-11-20 20:14:10 +0400

    tnayin

--- less.py
+++ less.py
@@ -1,13 +1,32 @@
-# խնդիր 136
+# խնդիր 136   split- ի մեջ ոնց մի քանի հրաման տամ
 
 
-x=input('===> ')
-dict = {
-    'anun' : 'Aram',
-    'mail' : 'a@maill.ru',
-    'tariq': 33,
-    'taretiv': 1989
-}
-print(dict[x])
+# x=input('===> ')
+# dict = {
+#     'anun' : 'Aram',
+#     'mail' : 'a@maill.ru',
+#     'tariq': 33,
+#     'taretiv': 1989
+# }
+# z =x.split()
+
+# for i in z:
+#     print(dict[i])
 
 
+# խնդիր 138
+
+# x = input('===> ')
+# list = []
+# dict = {
+#     '2' : 'A', 
+#     '22' : 'B',
+#     '222' : 'C',
+#     '3' : 'D',
+#     '33' : 'E',
+#     '333' : 'F',
+# }
+
+# z = x.split()
+# for i in z:
+#     print(dict[i])

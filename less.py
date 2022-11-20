6b84b1ccf6ab2f6b4cf6d8adef7206acb763ca4c
0hakob0 <0hakob0@gmail.com>
2022-11-20 16:24:14 +0400

    tnayin

--- less.py
+++ less.py
@@ -1,14 +1,5 @@
 import random
 dict = {
-    'Աշակերտ 1' : random.randint(1, 10),
-    'Աշակերտ 2' : random.randint(1, 10),
-    'Աշակերտ 3' : random.randint(1, 10),
-    'Աշակերտ 4' : random.randint(1, 10),
-    'Աշակերտ 5' : random.randint(1, 10),
-    'Աշակերտ 6' : random.randint(1, 10),
-    'Աշակերտ 7' : random.randint(1, 10),
-    'Աշակերտ 8' : random.randint(1, 10),
-    'Աշակերտ 9' : random.randint(1, 10),
-    'Աշակերտ 10' : random.randint(1, 10)
+    
     }
 print(dict)

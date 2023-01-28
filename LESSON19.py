dcd4c57d4b981eb143e179deb872ab856f3e4930
0hakob0 <mail>
2023-01-28 19:29:46 +0400

    12

---
+++ LESSON19.py
@@ -0,0 +1,73 @@
+# def pr():
+#     print("python")
+
+#     pr()
+# pr()
+
+
+# def say_hello():
+#     print("Hello")
+#
+# message = say_hello
+# message()
+
+# def sum(a, b):
+#     return a + b
+#
+# operation = sum
+# result = operation(6, 7)
+#
+# print(result)
+
+# def do_operation(a, b, operation):
+#     result = operation(a, b)
+#     print(f"result = {result}")
+#
+#
+# def sum(a, b):
+#     return a + b
+#
+# do_operation(5, 4, sum)
+
+
+
+
+"""8"""
+
+# ### maini hamar
+# def asd1():
+#     a = int(input('a==> '))
+#     b = int(input('b==> '))
+#     c = int(input('c==> '))
+#     if a % b > b % c:
+#         print('True')
+#     else:
+#         print('False')
+#
+#
+# asd1()
+#
+#
+# """10"""
+#
+#
+# def asd():
+#     a = int(input('a==> '))
+#     b = int(input('b==> '))
+#     c = int(input('c==> '))
+#     d = int(input('d==> '))
+#     list1 = [a, b, d]
+#     list1.sort()
+#     list1.insert(0, c)
+#     print(list1)
+#
+#
+# asd()
+#
+#
+# def main():
+#     asd()
+#     asd1()
+#
+#
+# main()

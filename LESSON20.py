3ba33f6d8c3887d620a62c74ffb38633c324d863
0hakob0 <mail>
2023-02-02 05:34:08 +0400

    12

--- LESSON20.py
+++ LESSON20.py
@@ -62,194 +62,224 @@
 """1"""
 
 
-# def task1():
-#     n = int(input('==> '))
-#
-#     def task11():
-#         nonlocal n
-#         while n != 0:
-#             x = n % 10
-#             y = n // 10
-#             n = y
-#             print(x)
-#     return task11
-#
-#
-# fn = task1()
-# fn()
+def task1():
+    n = int(input('==> '))
+
+    def task11():
+        nonlocal n
+        while n != 0:
+            x = n % 10
+            y = n // 10
+            n = y
+            print(x)
+    return task11
+
+
+fn = task1()
+fn()
 
 
 """2"""
 
 
-# def task1():
-#     n = int(input('==> '))
-#     z = 0
-#
-#     def task11():
-#         nonlocal n
-#         nonlocal z
-#         while n != 0:
-#             x = n % 10
-#             y = n // 10
-#             n = y
-#             z += 1
-#         print(z)
-#     return task11
-#
-#
-# fn = task1()
-# fn()
+def task1():
+    n = int(input('==> '))
+    z = 0
+
+    def task11():
+        nonlocal n
+        nonlocal z
+        while n != 0:
+            x = n % 10
+            y = n // 10
+            n = y
+            z += 1
+        print(z)
+    return task11
+
+
+fn = task1()
+fn()
 
 
 """3"""
 
 
-# def task1():
-#     n = int(input('==> '))
-#     z = 0
-#
-#     def task11():
-#         nonlocal n
-#         nonlocal z
-#         while n != 0:
-#             x = n % 10
-#             y = n // 10
-#             n = y
-#             z += x
-#         print(z)
-#     return task11
-#
-#
-# fn = task1()
-# fn()
+def task1():
+    n = int(input('==> '))
+    z = 0
+
+    def task11():
+        nonlocal n
+        nonlocal z
+        while n != 0:
+            x = n % 10
+            y = n // 10
+            n = y
+            z += x
+        print(z)
+    return task11
+
+
+fn = task1()
+fn()
 
 
 """4"""
 
 
-# def task1():
-#     n = int(input('==> '))
-#     z = 1
-#     def task11():
-#         nonlocal n
-#         nonlocal z
-#         while n != 0:
-#             x = n % 10
-#             y = n // 10
-#             n = y
-#             z *= x
-#         print(z)
-#     return task11
-#
-#
-# fn = task1()
-#
-# fn()
+def task1():
+    n = int(input('==> '))
+    z = 1
+    def task11():
+        nonlocal n
+        nonlocal z
+        while n != 0:
+            x = n % 10
+            y = n // 10
+            n = y
+            z *= x
+        print(z)
+    return task11
+
+
+fn = task1()
+
+fn()
 
 
 """7"""
 
 
-# def task1():
-#     n = int(input('==> '))
-#
-#     def task11():
-#         nonlocal n
-#         while n != 0:
-#             x = n % 10
-#             y = n // 10
-#             n = y
-#             print(x, end= '')
-#     return task11
-#
-#
-# fn = task1()
-# fn()
+def task1():
+    n = int(input('==> '))
+
+    def task11():
+        nonlocal n
+        while n != 0:
+            x = n % 10
+            y = n // 10
+            n = y
+            print(x, end= '')
+    return task11
+
+
+fn = task1()
+fn()
 
 
 """8"""
 
 
-# def task1():
-#     n = int(input('==> '))
-#
-#     def task11():
-#         nonlocal n
-#         while n != 0:
-#             x = n % 10
-#             y = n // 10
-#             n = y
-#             if x == 3:
-#                 print(True)
-#                 break
-#         else:
-#             print(False)
-#
-#     return task11
-#
-#
-# fn = task1()
-# fn()
+def task1():
+    n = int(input('==> '))
+
+    def task11():
+        nonlocal n
+        while n != 0:
+            x = n % 10
+            y = n // 10
+            n = y
+            if x == 3:
+                print(True)
+                break
+        else:
+            print(False)
+
+    return task11
+
+
+fn = task1()
+fn()
 
 
 """10"""
 
-#
-# def task1():
-#     n = int(input('==> '))
-#
-#     def task11():
-#         nonlocal n
-#         while n != 0:
-#             x = n % 10
-#             y = n // 10
-#             n = y
-#             if x % 2 == 0:
-#                 print(True)
-#                 break
-#         else:
-#             print(False)
-#
-#     return task11
-#
-#
-# fn = task1()
-# fn()
+
+def task1():
+    n = int(input('==> '))
+
+    def task11():
+        nonlocal n
+        while n != 0:
+            x = n % 10
+            y = n // 10
+            n = y
+            if x % 2 == 0:
+                print(True)
+                break
+        else:
+            print(False)
+
+    return task11
+
+
+fn = task1()
+fn()
 
 
 """11"""
 
 
-# def task1():
-#     n = int(input('==> '))
-#     z = 0
-#     l = 0
-#
-#     def task11():
-#         nonlocal n
-#         nonlocal z
-#         nonlocal l
-#         while n != 0:
-#             x = n % 10
-#             y = n // 10
-#             n = y
-#             if x % 2 != 0:
-#                 z += x
-#             elif x % 2 == 0:
-#                 l += x
-#             if z == l:
-#                 print(True)
-#                 break
-#         else:
-#             print(False)
-#
-#     return task11
-#
-#
-# fn = task1()
-# fn()
+def task1():
+    n = int(input('==> '))
+    z = 0
+    l = 0
+
+    def task11():
+        nonlocal n
+        nonlocal z
+        nonlocal l
+        while n != 0:
+            x = n % 10
+            y = n // 10
+            n = y
+            if x % 2 != 0:
+                z += x
+            elif x % 2 == 0:
+                l += x
+            if z == l:
+                print(True)
+                break
+        else:
+            print(False)
+
+    return task11
 
 
+fn = task1()
+fn()
 
+
+"""12"""
+
+
+def task1():
+    n = int(input('==> '))
+    z = 0
+    l = 0
+
+    def task11():
+        nonlocal n
+        nonlocal z
+        nonlocal l
+        while n != 0:
+            x = n % 10
+            y = n // 10
+            n = y
+            if x % 2 != 0:
+                z += 1
+            elif x % 2 == 0:
+                l += 1
+        if z == l:
+            print(True)
+
+        else:
+            print(False)
+
+    return task11
+
+
+fn = task1()
+fn()
 
